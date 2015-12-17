c18dc58a0b53026310453585a34da27ea2183739
Luis Garcia <luisgarciau@gmail.com>
2015-12-17 13:35:15 -0500

    Added a Windows Runtime Component "ObjectLocationProvider". This will serve as provider of object detection events.
    Modify Doxyfile to include the new directory added.
    
    Add Doxyfile to the solution, in that way the file can be modified
    easily within the solution.

---
+++ ObjectLocationProvider/Chart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLocationProvider
+{
+    /** Contains the information of the chart being tracked */
+    public sealed class Chart
+    {
+
+        
+    }
+}
